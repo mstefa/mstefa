5c2540f263a404f4156ff626a31df58543943ee0
mstefa <mstefanutti24@gmail.com>
2023-08-22 12:01:58 +0200

    feat: edit relative path and next.config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="shortcut icon" href="./favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   )
