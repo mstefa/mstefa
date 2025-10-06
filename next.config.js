fca41256a3ab168d9bebee1955088dc3cebbdf51
Matias Stefanutti <mstefanutti24@gmail.com>
2025-10-07 10:41:34 +1100

    fix: remove MDX for the moment

--- next.config.js
+++ next.config.js
@@ -16,7 +16,8 @@ const nextConfig = {
   }
 }
 
-const withMDX = require('@next/mdx')()
-module.exports = withMDX(nextConfig)
+// const withMDX = require('@next/mdx')()
+// module.exports = withMDX(nextConfig)
+module.exports = nextConfig
 
 
